8ada83dfde18b5dd73bb4bcb0e9d75bc6528c801
ifaz5 <studentifaz@gmail.com>
2021-10-19 22:19:33 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import BookingAppointMent from "./components/Home/BookingAppointment/BookingAppointMent";
+import Contacts from "./components/Home/Contacts/Contacts";
+import Home from "./components/Home/Home/Home";
+import OurDoctors from "./components/Home/OurDoctors/OurDoctors";
+import Service from "./components/Home/Service/Service";
+import ServiceDetail from "./components/ServiceDetail/ServiceDetail";
+import Login from "./components/Shared/Login/Login";
+import PrivetRoute from "./components/Shared/Login/PrivetRoute/PrivetRoute";
+import Navbar from "./components/Shared/Navbar/Navbar";
+import NotFound from "./components/Shared/NotFound/NotFound";
+import AuthProvider from "./Context/AuthProvider";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <Navbar></Navbar>
+        <Switch>
+
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <PrivetRoute path="/service">
+            <Service></Service>
+          </PrivetRoute>
+          <PrivetRoute path="/contact">
+            <Contacts />
+          </PrivetRoute>
+          <PrivetRoute path="/Our-Doctors">
+            <OurDoctors></OurDoctors>
+          </PrivetRoute>
+          <PrivetRoute path="/Booking-appointment">
+            <BookingAppointMent />
+          </PrivetRoute>
+          <PrivetRoute path="/ServiceDetail/:id">
+            <ServiceDetail></ServiceDetail>
+          </PrivetRoute>
+
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
+    </AuthProvider>
   );
 }
 
